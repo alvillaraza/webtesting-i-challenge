cf233cecca871e205db9c7a2dd8c58b420105006
alexis villaraza <alexis.borja@gmail.com>
2020-03-25 12:54:48 -0700

    got repair test to work

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,12 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
+const { repair } = require("./enhancer.js");
 // test away!
+describe("enhancer", function() {
+  describe("repair()", function() {
+    it("should return the item with a durability of 100", function() {
+      const repairedItem = repair({ item: "item", durability: 80 });
+
+      expect(repairedItem.durability).toBe(100);
+    });
+  });
+});
